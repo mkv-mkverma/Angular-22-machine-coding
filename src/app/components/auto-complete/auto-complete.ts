d7eef7b00532478b805936fd303483f4041bced2
manish verma <manishverma@manishs-MacBook-Pro.local>
2026-08-01 20:04:31 +0530

    autocomplete

---
+++ src/app/components/auto-complete/auto-complete.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
+
+@Component({
+  selector: 'app-auto-complete',
+  imports: [ReactiveFormsModule],
+  templateUrl: './auto-complete.html',
+  styleUrl: './auto-complete.scss',
+})
+export class AutoComplete {
+  private http = inject(HttpClient);
+
+  searchControl = new FormControl();
+
+  product$ = this.searchControl.valueChanges.pipe(
+    debounceTime(500),
+    map((value: any) => value.trim()),
+    distinctUntilChanged(),
+    filter((e) => e.length > 2),
+    switchMap((v) => (v ? this.getProductList(v) : of([]))),
+    map((e: any) => e.products),
+    tap((e) => console.log(e)),
+  );
+
+  productList = toSignal(this.product$);
+
+  getProductList(searchText: string) {
+    return this.http.get<any[]>(`https://dummyjson.com/products/search?q=${searchText}`);
+  }
+}

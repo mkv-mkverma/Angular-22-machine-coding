776e4072c921966da5bbe74b255dc58be7ace61c
manish verma <manishverma@manishs-MacBook-Pro.local>
2026-08-13 15:35:04 +0530

    env, injector token and polling

--- src/app/components/short-polling/short-polling.spec.ts
+++ src/app/components/short-polling/short-polling.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
+import { API_URL } from '../core/tokens/api-url.token';
 import { ShortPolling } from './short-polling';
 
 describe('ShortPolling', () => {
@@ -9,6 +12,10 @@ describe('ShortPolling', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ShortPolling],
+      providers: [
+        { provide: API_URL, useValue: 'https://jsonplaceholder.typicode.com' },
+        { provide: HttpClient, useValue: { get: () => of([]) } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ShortPolling);
